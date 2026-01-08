1196c318ac7fbbc9287324d1479ae29c058ae899
ysei-hash <y_sei@keio.jp>
2026-01-08 15:34:58 +0900

    B-12-4.py

--- B-12-4.py
+++ B-12-4.py
@@ -1,11 +1,10 @@
 import pyxel
 
-pyxel.init(200,200)
+pyxel.init(200, 200)
 pyxel.cls(7)
-for b  in range (10,200,20):
-    for a in range(10,200,20):
-        if ((a-10)/20 + (b-10)/20) %2 == 0:
-            pyxel.circ(a,b,10,6)
-    else:
-        pyxel.circ(a,b,10,14)
-pyxel.show()    
+
+for i in range(0, 200, 10):
+    pyxel.line(0, i, i, 199, 0)
+
+pyxel.show()
+
